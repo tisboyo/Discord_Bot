e4fa7525a1548d81973b957cacf4f774b0318223
Tisboyo <10230718+tisboyo@users.noreply.github.com>
2020-05-21 01:29:00 -0400

    Added minimum age of message requirement

--- cogs/temp_voice_channels.py
+++ cogs/temp_voice_channels.py
@@ -6,6 +6,8 @@ Discord Bot for HardwareFlare and others
 
 import logging
 import asyncio
+import datetime
+from collections import namedtuple
 
 import discord
 from discord.ext import commands
@@ -42,6 +44,11 @@ class TempVoice(commands.Cog):
         # Category for voice channels
         self.category = "Topical Voice Chat"
 
+        self.message_ttl_delta = datetime.timedelta(minutes=5)
+
+        for guild in client.guilds:
+            Database.Cogs[self.name][guild.id] = dict()
+
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
         # Have to reload Database.Cogs when joining a new guild to prevent errors.
@@ -67,23 +74,12 @@ class TempVoice(commands.Cog):
         ):
             return
 
-        try:
-            last_message = Database.Cogs[self.name][message.guild.id]
-        except KeyError:
-            last_message = Database.Cogs[self.name][message.guild.id] = dict()
+        last_message_channels = Database.Cogs[self.name][message.guild.id]
 
-        last_message[message.author.id] = message.channel
-
-    # @commands.group(hidden=True)
-    # @Permissions.check()
-    # async def Template(self, ctx):
-    #     """
-
-    # 	Default Permissions:
-    # 	"""
-    #     # Guard Clause
-    #     if ctx.guild == None:  # Not in a guild means DM or Group chat.
-    #         return
+        nt = namedtuple("last_message", "channel time")
+        last_message_channels[message.author.id] = nt(
+            channel=message.channel, time=datetime.datetime.now()
+        )
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
@@ -135,20 +131,10 @@ class TempVoice(commands.Cog):
         if before.channel is not None:  # Member left the voice channel
             await self.member_left_voice_channel(member, guild, before)
 
-    async def member_joined_lobby(self, member, guild, after):
-        """
-        Handles when a member joins the voice chat lobby
-        """
-        try:  # Get the last channels that messages have been sent in this guild.
-            last_message_channels = Database.Cogs[self.name][guild.id]
-        except KeyError:  # No messages have been sent in the guild yet.
-            last_message_channels = Database.Cogs[self.name][guild.id] = dict()
-
-        try:  # Get the last channel that this member sent a message in
-            last_message_channel = last_message_channels[member.id]
-        except:  # User doesn't have a last message sent.
-            # Bot may have been offline or rebooted since the last message.
-
+    async def dm_member(
+        self, member: discord.Member, expired_message: bool = False,
+    ):
+        if not expired_message:
             # DM the user a friendly note.
             await member.send(
                 "Well this is embarrassing ... :flushed:\r\n"
@@ -157,36 +143,107 @@ class TempVoice(commands.Cog):
                 "If you don't speak in a text channel in the next 60 seconds, I will "
                 "remove you from the Lobby."
             )
+        else:
+            await member.send(
+                f"I haven't seen a message from you in the last {self.message_ttl_delta}."
+                "Could you please send a message in the channel that you "
+                "would like to create. In 60 seconds you will be removed "
+                "from the Lobby."
+            )
 
-            # TODO MAYBE! If a denied error occurs when sending the DM,
-            # Join the voice channel as the bot and read them the message.
-            # Or maybe just do that in all cases.
+    async def get_last_message(
+        self,
+        member: discord.Member,
+        guild: discord.Guild,
+        expired_message: bool = False,
+    ):
 
-            # Wait and see if the user has said anything yet.
-            for _ in range(60, 0, -1):
-                await asyncio.sleep(1)
-                if last_message_channels.get(member.id, False):
-                    last_message_channel = last_message_channels[member.id]
+        await self.dm_member(member, expired_message=expired_message)
+
+        # Default to kicking the user, until we prove otherwise
+        kick_from_lobby = True
+
+        last_message_channels = Database.Cogs[self.name][guild.id]
+
+        # TODO MAYBE! If a denied error occurs when sending the DM,
+        # Join the voice channel as the bot and read them the message.
+        # Or maybe just do that in all cases.
+
+        # Wait and see if the user has said anything yet.
+        for _ in range(60, 0, -1):
+            await asyncio.sleep(1)
+
+            if last_message_channels.get(member.id, False):
+                last_message = last_message_channels[member.id]
+
+                # Message is in the last five minutes, so it's valid.
+                if not self.is_message_expired(last_message):
+                    kick_from_lobby = False
                     break
 
-            if not last_message_channels.get(member.id, False):
-                # User still hasn't joined a channel.
-                await member.move_to(None, reason="Idling in the Lobby")
+        if kick_from_lobby:
+            # No message in memory OR the message is more than 5 minutes old
+            await self.kick_from_voice_channel(member)
+            return None
 
+        return last_message
+
+    def is_message_expired(self, message) -> bool:
+        """Check if the message is expired"""
+
+        message_expires = datetime.datetime.now() - self.message_ttl_delta
+
+        if message_expires <= message.time:
+            return False
+        elif message_expires > message.time:
+            return True
+        else:
+            return None
+
+    async def kick_from_voice_channel(self, member: discord.Member):
+        await member.move_to(None, reason="Kicked by Bot")
+
+    async def member_joined_lobby(
+        self, member: discord.Member, guild: discord.Guild, after
+    ):
+        """
+        Handles when a member joins the voice chat lobby
+        """
+
+        last_message_channels = Database.Cogs[self.name][guild.id]
+
+        # Get the user last_message information
+        try:
+            # Retrieve the last message by the user
+            last_message = last_message_channels[member.id]
+
+        except KeyError:  # User doesn't have a last message sent.
+            # Bot may have been offline or rebooted since the last message.
+            last_message = await self.get_last_message(member, guild)
+
+            if last_message is None:
+                return
+
+        # Check to make sure the message is not more than 5 minutes old
+        if self.is_message_expired(last_message):
+            last_message = await self.get_last_message(member, guild, True)
+
+            # If last_message returns None, return.
+            if not last_message:
                 return
 
         category = self.get_category_by_name(guild, self.category)
 
         # Establish the permissions needed for the new channel
-        overwrites = last_message_channel.overwrites
+        overwrites = last_message.channel.overwrites
 
         # Check if the channel exists already
-        channel = self.get_voice_channel_by_name(guild, last_message_channel.name)
+        channel = self.get_voice_channel_by_name(guild, last_message.channel.name)
 
         if channel is None:  # Create it
             # TODO Add reason for creation
             channel = await guild.create_voice_channel(
-                last_message_channel.name, category=category, overwrites=overwrites
+                last_message.channel.name, category=category, overwrites=overwrites
             )
 
         # Checking to make sure the channel actually exists, in case of a creation error
@@ -210,6 +267,10 @@ class TempVoice(commands.Cog):
         if before.channel.category == self.get_category_by_name(guild, self.category):
             if len(before.channel.members) == 0:
                 text_channel = self.get_text_channel_by_name(guild, before.channel.name)
+
+                # Either a channel wasn't found, or the bot doesn't have permissions in it
+                if text_channel is None:
+                    return
 
                 await text_channel.send(
                     f"Voice channel `{before.channel.name}` is now empty, and will be deleted in 60 seconds. \r\n"
@@ -235,9 +296,14 @@ class TempVoice(commands.Cog):
         """
         Get text channel object by channel_name
         """
-        for c in guild.channels:
-            if c.name == channel_name and c.type == discord.ChannelType.text:
-                return c
+        for channel in guild.channels:
+            if (
+                channel.name == channel_name
+                and channel.type == discord.ChannelType.text
+            ):
+                # Check for bot permissions
+                if channel.permissions_for(guild.me).send_messages:
+                    return channel
 
         return None
 
